49a3a5ecf7c1e6596ccef188e80cff2917b46053
Peter Wacha <128045830+DINGZTRADER@users.noreply.github.com>
2026-08-19 14:49:01 +0300

    Restore YousefCreationz original WhatsApp number

--- app/data.js
+++ app/data.js
@@ -1,7 +1,7 @@
 export const STORE = {
   name: 'YousefCreationz',
-  whatsappDisplay: '0704 650 600',
-  whatsappIntl: '256704650600',
+  whatsappDisplay: '0758 704 993',
+  whatsappIntl: '256758704993',
   city: 'Kampala, Uganda',
   logo: '/api/product-image?id=logo'
 };
